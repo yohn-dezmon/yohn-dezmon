3ba80acde216f21d094abf154e6d2b8399e91621
John Desmond <johndesmond631@gmail.com>
2023-08-06 09:49:28 -0400

    successfully created data file with gitignore

--- GeoPandas/documentation/User-Guide/intro_to_geopandas.py
+++ GeoPandas/documentation/User-Guide/intro_to_geopandas.py
@@ -6,10 +6,11 @@ import geopandas
 from geodatasets import get_path 
 
 # a map of New York boroughs
-# path_to_data = get_path("nybb")
-gdf = geopandas.read_file("data/nybb.geojson")
+path_to_data = get_path("nybb")
+# "data/nybb.geojson"
+gdf = geopandas.read_file(path_to_data)
 
 print(gdf)
 
 # .geojson
-# gdf.to_file("data/nybb.geojson", driver="GeoJSON")
+gdf.to_file("data/nybb.geojson", driver="GeoJSON")
