9d878ac5d11af0c06c5911d91d1e655fe569f811
John-Desmond <john.desmond@gro-intelligence.com>
2023-08-09 23:33:59 +0100

    finished set operations page

--- GeoPandas/documentation/User-Guide/set-operations/groceries_ex.py
+++ GeoPandas/documentation/User-Guide/set-operations/groceries_ex.py
@@ -15,6 +15,18 @@ groceries = read_file("data/groceries.geojson")
 chicago = chicago.to_crs("ESRI:102003")
 groceries = groceries.to_crs("ESRI:102003")
 
-chicago.plot()
-plt.show()
+# chicago.plot()
+# plt.show()
 
+# buffer all grocery geometries by 1 km 
+groceries['geometry']= groceries.buffer(1000)
+groceries.plot()
+plt.show()
+# get the intersecting areas of chicago with the buffered groceries 
+chicago_cores = chicago.overlay(groceries, how='intersection')
+chicago_cores.plot(alpha=0.5, edgecolor='k', cmap='tab10')
+
+"""
+Not sure why but I'm unable to view the groceries data
+it only works if I dl it from the library
+"""
